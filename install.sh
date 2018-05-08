aafeb3063a991f5cf4c29c8c1754b5030c19f8ff
Unknown <segler_alex@web.de>
2018-05-08 22:13:04 +0200

    added install sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+cargo build --release
+sudo mkdir -p /usr/local/bin
+
+NAME="update-caches"
+sudo cp target/release/radiobrowser-cleanup-rust "/usr/local/bin/${NAME}.bin"
+sudo cp "init/${NAME}.service" /etc/systemd/system
+
+cat <<EOF | sudo tee "/usr/local/bin/${NAME}"
+#!/bin/bash
+. /etc/${NAME}.conf
+export PAUSE_SECONDS
+export LOOP
+export DATABASE_URL
+${NAME}.bin
+EOF
+
+CONFIG_FILE="/etc/${NAME}.conf"
+if [ ! -f "${CONFIG_FILE}" ]; then
+    cat <<EOF | sudo tee "/etc/${NAME}.conf"
+# pause between database checks if no stations to check are available
+PAUSE_SECONDS=60
+# continue after STATIONS stations have been checked
+LOOP=true
+# database connection string (mysql, mariadb)
+DATABASE_URL=mysql://myuser:mypassword@localhost/radio
+EOF
+fi
+
+sudo chmod ugo+x "/usr/local/bin/${NAME}"
+sudo groupadd --system streamcheck
+sudo useradd --system --no-create-home --gid streamcheck streamcheck
+
+sudo systemctl daemon-reload
+
+echo "Enable service with:"
+echo " - systemctl enable ${NAME}"
+echo "Start service with:"
+echo " - systemctl start ${NAME}"
+echo "Logs:"
+echo " - journalctl log -uf ${NAME}"
+echo "Edit /etc/${NAME}.conf according to your needs."
